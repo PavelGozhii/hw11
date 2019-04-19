4530a88224a14a1bc41134acca5f19bd878a641d
PavelGozhii <36234557+PavelGozhii@users.noreply.github.com>
2019-04-19 17:22:26 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,50 +1,37 @@
 package com.company;
 
-import javafx.util.Pair;
-
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
-                "Sed sodales consectetur purus at faucibus. Donec mi quam, tempor vel ipsum non, " +
-                "faucibus suscipit massa. Morbi lacinia velit blandit tincidunt efficitur. Vestibulum " +
-                "eget metus imperdiet sapien laoreet faucibus. Nunc eget vehicula mauris, ac auctor " +
-                "lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel " +
-                "odio nec mi tempor dignissim.";
-        String[] answer = getMostInputWords(str);
+        String[] answer = newGetMostInputWordsN(in.nextLine());
         Arrays.stream(answer).forEachOrdered(System.out::println);
     }
 
-    public static String[] getMostInputWords(String inputString) {
-        inputString = inputString.replaceAll("[,.]", "").toLowerCase();
-        String[] stringArr = inputString.split(" ");
-        List<Pair<String, Integer>> pairList = new ArrayList();
-        for (String currentString : stringArr) {
-            int count = (int) Arrays.stream(stringArr).filter(currentString::equals).count();
-            pairList.add(new Pair<>(currentString, count));
-        }
-        Comparator<Pair<String, Integer>> comparator = Comparator.comparing(Pair::getValue, Comparator.reverseOrder());
-        comparator = comparator.thenComparing(Pair::getKey);
-        pairList.sort(comparator);
-        pairList = removeDuplicates((ArrayList<Pair<String, Integer>>) pairList);
-        String[] answer = new String[10];
-        for (int i = 0; i < 10; i++) {
-            answer[i] = pairList.get(i).getKey();
-        }
-        return answer;
-    }
 
-    private static ArrayList removeDuplicates(ArrayList list) {
-        ArrayList newList = new ArrayList();
-        for (Object element : list) {
-            if (!newList.contains(element)) {
-                newList.add(element);
-            }
-        }
-        return newList;
+    public static String[] newGetMostInputWordsN(String inputString) {
+        List<String> answer = Stream.of(inputString.toLowerCase().split("(?:[!,.?^_]|-)"))
+                .flatMap(n -> Arrays.asList(n.split(" ")).stream())
+                .collect(Collectors.toList()).stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .collect(
+                        Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (n1, n2) -> {
+                                    throw new IllegalStateException();
+                                },
+                                LinkedHashMap::new)).keySet().stream().limit(10)
+                .collect(Collectors.toList());
+
+        String[] strings = new String[10];
+        IntStream.range(0, answer.size()).forEach(i -> strings[i] = answer.get(i));
+        return strings;
     }
 
 }
